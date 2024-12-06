3a64753148c70a1320d615e9262d787252e35749
seorinchoi <89957826+seorinchoi@users.noreply.github.com>
2024-12-06 18:52:00 +0900

    Update __init__.py

--- segm/data/__init__.py
+++ segm/data/__init__.py
@@ -4,3 +4,5 @@ from segm.data.imagenet import ImagenetDataset
 from segm.data.ade20k import ADE20KSegmentation
 from segm.data.pascal_context import PascalContextDataset
 from segm.data.cityscapes import CityscapesDataset
+from segm.data.smc import SMCSegmentation
+
